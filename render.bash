192bb3211f1cae8ca4ba459793dc4c0dc5a2a94d
Michael R. Crusoe <michael.crusoe@gmail.com>
2023-04-17 18:34:16 +0200

    add render script

---
+++ render.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [[ -z "$WORKSPACE" ]] ; then
+    WORKSPACE=$PWD/temp/
+fi
+mkdir -p $WORKSPACE
+git clone https://github.com/common-workflow-language/cwl-website.git $WORKSPACE/cwl-website
+CURDIR=$PWD
+pushd $WORKSPACE/cwl-website ; ln -s $CURDIR v1.2 ; popd
+cp cwlsite-v1.2-only-job.yaml $WORKSPACE/cwl-website/site/
+mkdir -p render
+cwltool $@ --cache $WORKSPACE/cache --outdir $PWD/render --relax-path-checks $WORKSPACE/cwl-website/site/cwlsite.cwl $WORKSPACE/cwl-website/site/cwlsite-v1.2-only-job.yaml
