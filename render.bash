c928c987aa53832e0e0c117861105379d4bfa601
Michael R. Crusoe <1330696+mr-c@users.noreply.github.com>
2023-04-18 07:51:47 +0200

    render: re-use git checkout

--- render.bash
+++ render.bash
@@ -3,7 +3,13 @@ if [[ -z "$WORKSPACE" ]] ; then
     WORKSPACE=$PWD/temp/
 fi
 mkdir -p $WORKSPACE
-git clone https://github.com/common-workflow-language/cwl-website.git $WORKSPACE/cwl-website
+repo=https://github.com/common-workflow-language/cwl-website \
+bn=$(basename $repo)
+if [[ -d $bn ]] ; then
+    (cd $bn && git fetch origin && git reset --hard origin/main)
+else
+    git clone $repo && pushd $bn; git checkout main ; git show --no-patch ; popd
+fi
 CURDIR=$PWD
 pushd $WORKSPACE/cwl-website ; ln -s $CURDIR v1.2 ; popd
 cp cwlsite-v1.2-only-job.yaml $WORKSPACE/cwl-website/site/
