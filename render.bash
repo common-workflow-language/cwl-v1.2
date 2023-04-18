f147549ae5c95c0d06872d8e8f8557f161c16180
GlassOfWhiskey <iacopo.c92@gmail.com>
2023-04-18 12:04:59 +0200

    Removed DocType from Map and Union

--- render.bash
+++ render.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ -z "$WORKSPACE" ]] ; then
-    WORKSPACE=$PWD/temp/
+    WORKSPACE=$PWD
 fi
 mkdir -p $WORKSPACE
 repo=https://github.com/common-workflow-language/cwl-website \
@@ -13,7 +13,6 @@ fi
 echo "- basename: Base.yml"  >> $WORKSPACE/cwl-website/site/v1.2-deps.yaml
 echo "  class: File"  >> $WORKSPACE/cwl-website/site/v1.2-deps.yaml
 echo "  location: ../v1.2/Base.yml" >> $WORKSPACE/cwl-website/site/v1.2-deps.yaml
-cat $WORKSPACE/cwl-website/site/v1.2-deps.yaml
 CURDIR=$PWD
 pushd $WORKSPACE/cwl-website ; ln -s $CURDIR v1.2 ; popd
 cp cwlsite-v1.2-only-job.yaml $WORKSPACE/cwl-website/site/
